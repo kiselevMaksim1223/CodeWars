6768310bb65d477de96fcfbfd2c410320f0becbc
Maksim_Kiselev <laurkurt@gmail.com>
2022-11-02 11:54:42 +0300

    some array

---
+++ 04/04.js
@@ -0,0 +1,23 @@
+function getLength(arr){
+    //return length of arr
+    return arr.length;
+  }
+  function getFirst(arr){
+    //return the first element of arr
+    return arr[0];
+  }
+  function getLast(arr){
+    //return the last element of arr
+    return arr[arr.length-1];
+  }
+  function pushElement(arr){
+    var el=1;
+    //push el to arr
+    
+    return arr.push(el1);
+  }
+  function popElement(arr){
+    //pop an element from arr
+    
+    return arr.pop();
+  }
