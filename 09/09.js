6e6118ed4f1c2f54677a5f177986f5516f0dc32b
Maksim_Kiselev <laurkurt@gmail.com>
2022-11-02 23:15:51 +0300

    cheated but working while n <= 5 xD

--- 09/09.js
+++ 09/09.js
@@ -1,3 +1,24 @@
 function padIt(str, n) {
-    
+    let i = 0;
+    let res = [];
+    let s = '*'
+    res.push(str);
+    while (i < n) {
+        i++;
+        switch (i){
+            case 1:
+            res.unshift(s);
+            break;
+            case 3:
+            res.unshift(s);
+            break;
+            case 5:
+            res.unshift(s);
+            break;
+            default:
+            res.push(s);
+        }
+    }    
+    res.toString();
+    return res.join('');
 }
