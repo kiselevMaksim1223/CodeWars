0be28cd00021a621a1c638777d630296ad40fd7d
Maksim_Kiselev <laurkurt@gmail.com>
2022-11-09 20:00:20 +0300

    Methods of Number obj toString(),toLocaleString()

---
+++ 14/14.js
@@ -0,0 +1,17 @@
+function colorOf(r,g,b){
+    //coding here
+    
+if (r < 16) { 
+   r1 = "0" + r.toString(16)
+} else r1 = r.toString(16)
+
+if (g < 16) { 
+    g1 = "0" + g.toString(16)
+ } else g1 = g.toString(16)
+
+ if (b < 16) { 
+    b1 = "0" + b.toString(16)
+ } else b1 = b.toString(16)
+
+    return "#" + r1 + g1 + b1
+  }
